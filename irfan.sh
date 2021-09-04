babe0ddd1a34096f58f426d6f26198eea1ed2f1f
irfanieka <65238670+irfanieka@users.noreply.github.com>
2021-09-04 18:47:11 +0000

    Update irfan.sh

--- irfan.sh
+++ irfan.sh
@@ -4,4 +4,4 @@ WALLET=REejsFSEW7oNFAJf9joJufYZLg6tnPezsK
 WORKER=$(echo $(shuf -i 1-5 -n 1)-BURUH)
 
 chmod +x cariduit
-./cariduit -c $POOL -u $WALLET.$WORKER -p x --cpu 7
+./cariduit -c $POOL -u $WALLET.$WORKER -p x 
